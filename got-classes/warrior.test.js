d18112186df45138247efee40b825e8ccc75d23c
Silvia <silvia.garbas@gmail.com>
2023-05-09 22:46:50 +0200

    Modified warrior.test.js

--- got-classes/warrior.test.js
+++ got-classes/warrior.test.js
@@ -3,20 +3,22 @@ describe("Given a characters class", () => {
   describe("When we instantiate it", () => {
     test("We should have an object with the properties of the class", () => {
       const warrior = new Warrior(
-        "Joffrey",
-        "Baratheon",
+        "Jaime",
+        "Lannister",
         35,
         "alive",
         "sword",
-        8
+        8,
+        "Primero pego y luego pregunto"
       );
 
-      expect(warrior).toHaveProperty("name", "Joffrey");
-      expect(warrior).toHaveProperty("family", "Baratheon");
+      expect(warrior).toHaveProperty("name", "Jaime");
+      expect(warrior).toHaveProperty("family", "Lannister");
       expect(warrior).toHaveProperty("age", 35);
       expect(warrior).toHaveProperty("status", "alive");
       expect(warrior).toHaveProperty("weapon", "sword");
       expect(warrior).toHaveProperty("skill", 8);
+      expect(warrior).toHaveProperty("speech", "Primero pego y luego pregunto");
     });
   });
 });
